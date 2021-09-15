062fe4a5c60c161081196845df42b1b8ed242103
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-09-14 17:30:12 -0700

    Add factory tuning library from Surge

--- src/gui/TuningMenu.cpp
+++ src/gui/TuningMenu.cpp
@@ -1,5 +1,43 @@
 #include "TuningMenu.h"
 
+namespace
+{
+File getFactoryTuningLibrary()
+{
+    auto getLibraryPath = [] (const File& rootPath, const File& usrPath, const String& libPath)
+    {
+        auto libraryDir = rootPath.getChildFile (libPath);
+        if (libraryDir.isDirectory())
+            return libraryDir;
+
+        libraryDir = usrPath.getChildFile (libPath);
+        if (libraryDir.isDirectory())
+            return libraryDir;
+
+        return File();
+    };
+
+#if JUCE_WINDOWS
+    const String libraryPath = "Library/Application Support/ChowKick/tuning_library";
+    const auto rootPath = File ("/");
+    const auto usrPath = File::getSpecialLocation (File::userHomeDirectory);
+    return getLibraryPath (rootPath, usrPath, libraryPath);
+#elif JUCE_MAC
+    const String libraryPath = "Library/Application Support/ChowKick/tuning_library";
+    const auto rootPath = File ("/");
+    const auto usrPath = File::getSpecialLocation (File::userHomeDirectory);
+    return getLibraryPath (rootPath, usrPath, libraryPath);
+#elif JUCE_LINUX
+    const String libraryPath = "usr/share/ChowKick/tuning_library";
+    const auto rootPath = File ("/");
+    const auto usrPath = File::getSpecialLocation (File::userHomeDirectory);
+    return getLibraryPath (rootPath, usrPath, libraryPath);
+#else
+    return File();
+#endif
+}
+}
+
 TuningMenu::TuningMenu (Trigger& trig) : trigger (trig)
 {
     trigger.addListener (this);
@@ -16,17 +54,18 @@ TuningMenu::~TuningMenu()
 
 void TuningMenu::refreshMenu()
 {
-    constexpr auto fileChooserFlags = FileBrowserComponent::openMode | FileBrowserComponent::canSelectFiles;
-
+    // constexpr auto fileChooserFlags = FileBrowserComponent::openMode | FileBrowserComponent::canSelectFiles;
     auto* rootMenu = getRootMenu();
     rootMenu->clear();
+
+    auto factoryTuningPath = getFactoryTuningLibrary();
 
     auto sclName = trigger.getScaleName();
     auto scaleOption = "Select SCL" + (sclName.isEmpty() ? "" : " (" + sclName + ")");
     rootMenu->addItem (scaleOption, [=]
                        {
                            resetMenuText();
-                           FileChooser chooser ("Choose Scale", File(), "*.scl");
+                           FileChooser chooser ("Choose Scale", factoryTuningPath, "*.scl");
                            if (chooser.browseForFileToOpen())
                                trigger.setScaleFile (chooser.getResult());
                        });
@@ -36,13 +75,20 @@ void TuningMenu::refreshMenu()
     rootMenu->addItem (mappingOption, [=]
                        {
                            resetMenuText();
-                           FileChooser chooser ("Choose Mapping", File(), "*.kbm");
+                           FileChooser chooser ("Choose Keyboard Mapping", factoryTuningPath, "*.kbm");
                            if (chooser.browseForFileToOpen())
                                trigger.setMappingFile (chooser.getResult());
                        });
 
-    rootMenu->addItem ("Reset to 12TET", [=]
+    rootMenu->addItem ("Reset to Standard (12TET)", [=]
                        { trigger.resetTuning(); });
+
+    rootMenu->addSeparator();
+    if (factoryTuningPath != File())
+    {
+        rootMenu->addItem ("Open Factory Tuning Directory", [=]
+                           { factoryTuningPath.startAsProcess(); });
+    }
 
     resetMenuText();
 }
